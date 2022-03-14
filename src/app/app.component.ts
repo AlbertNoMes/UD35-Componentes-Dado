e78ed38c7e0aa6a443aab520f379a58ff55d844f
AlbertNoMes <albertnotariomestres@gmail.com>
2022-03-14 23:27:55 +0100

    commit dado 1 y 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'UD35-Componentes-Dado';
+
+  /*A PARTIR DE AQUI ES EL EJEMPLO 2*/
+  valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado: string | undefined;
+
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1 == this.valor2 && this.valor1 == this.valor3)
+      this.resultado = 'Ganó';
+    else this.resultado = 'Perdió';
+  }
 }
--- src/app/dado/dado.component.ts
+++ src/app/dado/dado.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dado',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DadoComponent implements OnInit {
 
+  /*SEGUNDO EJEMPLO*/
+  /*SI SE QUE VOY A RECIBIR INFORMACION DE OTRO COMPONENTE AL COMPONENTE HIJO, UTILIZAMOS EL SELECTOR @Input*/
+  @Input() valor: number | string | undefined; /*TAMBIEN SE PUEDE PONER any*/
+
+  /*PRIMER EJEMPLO*/
+  /*valor: number = 0;*/
+
   constructor() { }
 
+  /*CUANDO LANCEMOS EL COMPONENTE, AUTOMATICAMENTE LANZARA ESTE METODO (OnInit)*/
+
+   /*PRIMER EJEMPLO*/
+  /*ngOnInit() {*/
+    /*NUMERO ALEATORIO ENTRE 1 Y 6*/
+    /*TRUNCATE PARA REDONDEAR*/
+    /*this.valor = Math.trunc(Math.random() * 6) + 1;*/
+  /*}*/
+
+  /*SEGUNDO EJEMPLO*/
   ngOnInit(): void {
+      /*EL METODO DE GENERAR LOS NUMS ALEATORIOS LO CALCULAMOS EN EL COMPONENTE PADRE Y LO ENVIAMOS COMO VALOR AL COMPONENTE HIJO*/
   }
 
 }
